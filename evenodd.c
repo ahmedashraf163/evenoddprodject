be4ef9ddab3eb3e7d91052e6a99efe3d9fbc6b19
ashraf <aashraf@mabaat.com>
2023-06-16 23:21:13 +0300

    i add new code

--- evenodd.c
+++ evenodd.c
@@ -1,20 +1,40 @@
-/* Program to check whether the input integer number 
- * is even or odd using the modulus operator (%)
- */
-#include<stdio.h>
-int main()
-{
-   // This variable is to store the input number 
-   int num;
- 
-   printf("Enter an integer: ");
-   scanf("%d",&num);
- 
-   // Modulus (%) returns remainder
-   if ( num%2 == 0 )
-      printf("%d is an even number", num);
-   else
-      printf("%d is an odd number", num);
- 
-   return 0;
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int arr[100];
+    int even[100];
+    int odd[100];
+    int n;
+    int i;
+    int j = 0;
+    int k = 0;
+    
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    for(i=0;i<n;i++){
+        if (arr[i]%2==0){
+            even[j] = arr[i];
+            j++;
+        }
+        else{
+            odd[k]=arr[i];
+            k++;
+        }
+    }
+    
+    for(i=0;i<j;i++){
+        printf("Even number are %d \n",even[i]);
+    }
+    
+    
+    for(i=0;i<k;i++){
+        printf("Odd Numbers are %d \n",odd[i]);
+    }
+    
+
 }
